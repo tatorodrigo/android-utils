efb363cae5d7d66f6438ca1d31d6ec7994af85c2
Rodrigo Botelho <tato.rodrigo@gmail.com>
2016-07-27 10:44:29 -0300

    utility to check file type as image, audio and video

--- utils/src/main/java/br/com/tattobr/android/utils/MimeTypeUtil.java
+++ utils/src/main/java/br/com/tattobr/android/utils/MimeTypeUtil.java
@@ -2,6 +2,8 @@ package br.com.tattobr.android.utils;
 
 import android.webkit.MimeTypeMap;
 
+import java.io.File;
+
 public class MimeTypeUtil {
     public static String getMimeType(String url) {
         String type = null;
@@ -13,6 +15,37 @@ public class MimeTypeUtil {
         if (extension != null) {
             type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
         }
-        return type;
+        return type != null ? type.toLowerCase() : null;
+    }
+
+    public static String getMimeType(File file) {
+        return getMimeType(file.getAbsolutePath());
+    }
+
+    public static boolean isImage(String url) {
+        String mimeType = getMimeType(url);
+        return mimeType != null && mimeType.startsWith("image");
+    }
+
+    public static boolean isImage(File file) {
+        return isImage(file.getAbsolutePath());
+    }
+
+    public static boolean isVideo(String url) {
+        String mimeType = getMimeType(url);
+        return mimeType != null && mimeType.startsWith("video");
+    }
+
+    public static boolean isVideo(File file) {
+        return isVideo(file.getAbsolutePath());
+    }
+
+    public static boolean isAudio(String url) {
+        String mimeType = getMimeType(url);
+        return mimeType != null && mimeType.startsWith("audio");
+    }
+
+    public static boolean isAudio(File file) {
+        return isAudio(file.getAbsolutePath());
     }
 }
